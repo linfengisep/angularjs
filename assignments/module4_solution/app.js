88ca9bc1efa33129b9e6d667683379a2ca3d2f17
linfeng <lwang@bouyguestelecom.com>
2020-08-20 11:54:46 +0200

    updating lib places

--- assignments/module4_solution/app.js
+++ assignments/module4_solution/app.js
@@ -1,4 +1,17 @@
 (function(){
     'use strict'
-    angular.module('MenuApp',[])
+    angular.module('MenuApp',['ui.router']);
+
+    angular.module('MenuApp').config(RoutesConfig)
+
+
+    RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+    function RoutesConfig($stateProvider,$urlRouterProvider){
+        $urlRouterProvider.otherwise('/tab1');
+
+        $stateProvider.state('/tab1',{
+            url:'/tab1',
+            template:'<div>This is TAB 1 here</div>'
+        });
+    }
 })()
